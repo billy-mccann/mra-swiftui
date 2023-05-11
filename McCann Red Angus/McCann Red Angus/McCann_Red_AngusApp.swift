70e55d8f837738e625172d6a8dbf0857f71e78e2
Bill McCann <hyperfine262@gmail.com>
2023-05-11 15:53:18 -0700

    initial commit

---
+++ McCann Red Angus/McCann Red Angus/McCann_Red_AngusApp.swift
@@ -0,0 +1,17 @@
+//
+//  McCann_Red_AngusApp.swift
+//  McCann Red Angus
+//
+//  Created by Bill McCann on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct McCann_Red_AngusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
